3bb68226ebcb1a7f957b7284b6b175c343242aec
Daniel Pham <pham.dany@gmail.com>
2015-04-11 17:55:38 -0400

    make a properly working install file

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-ln -s ackrc ~/.ackrc
-ln -s bash_profile ~/.bash_profile
-ln -s bin ~/bin
-ln -s gitconfig ~/.gitconfig
-ln -s lessfilter ~/.lessfilter
-ln -s vim ~/.vim
-ln -s vimrc ~/.vimrc
+for h in {ackrc,aliases,bash_profile,bash_prompt,functions,gitconfig,gitignore,lessfilter,vim,vimrc}; do
+    if [ ! -d ~/.$h ]; then
+        ln -s $(pwd)/$h ~/.$h
+    else
+        echo ln: ~/.$h: File exists.
+    fi
+done
+
+for v in bin; do
+    if [ ! -d ~/$v ]; then
+        ln -s $(pwd)/$v ~/$v
+    else
+        echo ln: ~/$v: File exists.
+    fi
+done
